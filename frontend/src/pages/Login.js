6f3ae70679d835afc46b3c23641e21c2421c62ed
Ossai Lucky <luckyblaqy@gmail.com>
2022-09-13 05:30:01 +0100

    created login functionality

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 const Login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const {login, error, isLoading} = useLogin()
 
     const handleSubmit = async (e) =>{
         e.preventDefault();
 
-        console.log(email, password)
+        await login(email, password)
+
     }
 
   return (
@@ -26,7 +29,8 @@ const Login = () => {
         value={password} 
         /> 
 
-        <button type='submit'>Sign up</button>
+        <button disabled={isLoading}>Log in</button>
+        {error && <div className="error">{error}</div>}
     </form>
   )
 }
